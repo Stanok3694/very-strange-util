14b552ee48206382946d8221608eac6c91360e3f
Stanislav Okunev <stanok3694@yandex.ru>
2017-11-05 19:02:32 +0300

    updated func which get even and uneven elements + update tests + check how it works with string

--- src/logic/simpleCrypt.v1.test.js
+++ src/logic/simpleCrypt.v1.test.js
@@ -2,6 +2,9 @@ describe('basic layer functional', () => {
     const x = 0.4;
     const y = 1.25;
     const arr = ['h', 'e', 'l', 'l', 'o', 'w', 'o', 'r', 'l', 'd'];
+    const evenElements = ['e', 'l', 'w', 'r', 'd'];
+    const unevenElements = ['h', 'l', 'o', 'o', 'l'];
+    const str = 'helloworld';
 
     const powOfSum = (x, y) => roundUpResult(Math.pow(x, 2) + 2*x*y + Math.pow(y, 2));
     const powOfResidual = (x, y) => roundUpResult(Math.pow(x, 2) - 2*x*y + Math.pow(y, 2));
@@ -38,13 +41,27 @@ describe('basic layer functional', () => {
     
     it('should take only even elements', () => {
         const actual = JSON.stringify(getArrPart(arr, 'even'));
-        const expected = JSON.stringify(['e', 'l', 'w', 'r', 'd']);
+        const expected = JSON.stringify(evenElements);
         expect(actual).toBe(expected);
     });
 
     it('should take only uneven elements', () => {
         const actual = JSON.stringify(getArrPart(arr, 'uneven'));
-        const expected = JSON.stringify(['h', 'l', 'o', 'o', 'l']);
+        const expected = JSON.stringify(unevenElements);
+        expect(actual).toBe(expected);
+    });
+
+    test('that it works fine with string (even)', () => {
+        const targetData = Array.from(str);
+        const actual = JSON.stringify(getArrPart(targetData, 'even'));
+        const expected = JSON.stringify(evenElements);
+        expect(actual).toBe(expected);
+    });
+
+    test('that it works fine with string (uneven)', () => {
+        const targetData = Array.from(str);
+        const actual = JSON.stringify(getArrPart(targetData, 'uneven'));
+        const expected = JSON.stringify(unevenElements);
         expect(actual).toBe(expected);
     });
 });
