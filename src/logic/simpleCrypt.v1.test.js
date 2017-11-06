52ddf8dd70798df8d8d7c9a37fc0a957a9d3e235
Stanislav Okunev <stanok3694@yandex.ru>
2017-11-06 20:00:54 +0300

    updated tests + fomatted + added  new func for average word length calculaton [RED_COMMIT]

--- src/logic/simpleCrypt.v1.test.js
+++ src/logic/simpleCrypt.v1.test.js
@@ -14,15 +14,40 @@ describe('basic layer functional', () => {
     // ToDo #2: can have spaces - need check it and change mechanism
     const targetPhrase = 'helloworld';
     const arr = Array.from(targetPhrase);
-    
+
     const evenPhrase = 'elwrd';
     const evenElements = Array.from(evenPhrase);
-    const unevenPhrase =  'hlool';
+    const unevenPhrase = 'hlool';
     const unevenElements = Array.from(unevenPhrase);
-    
+
     // ToDo #3: probably can move it to class property?
     const alphabeticalStr = 'abcdefghijklmnopqrstuvwxyz';
     const alphabeticalArr = Array.from(alphabeticalStr);
+
+    const getAverageWordLength = phrase => {
+        let listenFirstPosition = 0;
+        let target = phrase;
+        const toArr = Array.from(phrase);
+        console.log(toArr);
+        const res = toArr.map((e, i) => {
+            if (e === ' ' || i === toArr.length - 1) {
+                console.log(i);
+                console.log(e);
+                console.log(listenFirstPosition);
+
+                const newStr = target.substring(listenFirstPosition, i);
+                console.log(newStr);
+
+                target = target.substring(i);
+                console.log(target);
+
+                listenFirstPosition = i;
+                return newStr;
+            }
+        });
+        console.log(res);
+        return res;
+    };
 
     it('should calc pow of sum', () => {
         const actual = PowsService.Sum;
@@ -41,7 +66,7 @@ describe('basic layer functional', () => {
         const expected = -1.4025;
         expect(actual).toBe(expected);
     });
-    
+
     it('should take only even elements', () => {
         const actual = JSON.stringify(getArrPart(arr, 'even'));
         const expected = JSON.stringify(evenElements);
@@ -64,20 +89,20 @@ describe('basic layer functional', () => {
     test('that it works fine with string (even)', () => {
         const targetData = Array.from(targetPhrase);
         const actualArr = getArrPart(targetData, 'even');
-        
+
         const actual = prettifyArrAsStr(actualArr)
         const expected = prettifyArrAsStr(evenElements);
-        
+
         expect(actual).toBe(expected);
     });
 
     test('that it works fine with string (uneven)', () => {
         const targetData = Array.from(targetPhrase);
         const actualArr = getArrPart(targetData, 'uneven');
-        
+
         const actual = prettifyArrAsStr(actualArr);
         const expected = prettifyArrAsStr(unevenElements);
-        
+
         expect(actual).toBe(expected);
     });
 
@@ -92,7 +117,7 @@ describe('basic layer functional', () => {
     test('that it works with uneven elements arr', () => {
         const step = 3;
         const expected = 'korro';
-        
+
         const actualArr = unevenElements.map(e => shiftSymbol(e, step, alphabeticalArr));
         const actual = prettifyArrAsStr(actualArr);
         // console.log(actual);
@@ -106,6 +131,13 @@ describe('basic layer functional', () => {
         const actualArr = evenElements.map(e => shiftSymbol(e, step, alphabeticalArr));
         const actual = prettifyArrAsStr(actualArr);
         // console.log(actual);
+        expect(actual).toBe(expected);
+    });
+
+    it('return average word length', () => {
+        const phrase = "hey you man";
+        const expected = 3;
+        const actual = getAverageWordLength(phrase);
         expect(actual).toBe(expected);
     });
 });
